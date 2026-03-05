5a465b4e714a7ad33a9adf380bfeeecb078d4b97
Magier Marcin <marcinmagier@gmail.com>
2026-03-05 20:10:49 +0100

    Rename toolchain for embedded arm projects

--- cmake/toolchain_arm-none-eabi.cmake
+++ cmake/toolchain_arm-none-eabi.cmake
@@ -7,8 +7,16 @@
 set(TARGET_ARCH             arm)
 set(TARGET_TOOLCHAIN        arm-none-eabi)
 
+# check toolchain directory
 if(NOT TARGET_TOOLCHAIN_DIR)
-    set(TARGET_TOOLCHAIN_DIR    /opt/toolchain/${TARGET_TOOLCHAIN})
+    set(TARGET_TOOLCHAIN_DIR $ENV{TARGET_TOOLCHAIN_DIR})
+endif()
+if(NOT TARGET_TOOLCHAIN_DIR)
+    if(WIN32)
+        set(TARGET_TOOLCHAIN_DIR    C:/toolchain/${TARGET_TOOLCHAIN})
+    else()
+        set(TARGET_TOOLCHAIN_DIR    /opt/toolchain/${TARGET_TOOLCHAIN})
+    endif()
 endif()
 if(NOT EXISTS ${TARGET_TOOLCHAIN_DIR})
     message("Toolchain directory not found.")
@@ -16,19 +24,30 @@ if(NOT EXISTS ${TARGET_TOOLCHAIN_DIR})
     message(FATAL_ERROR "")
 endif()
 
+# make sure exe suffix is set for windows
+if(NOT CMAKE_HOST_EXECUTABLE_SUFFIX)
+    if(WIN32)
+        set(CMAKE_HOST_EXECUTABLE_SUFFIX ".exe")
+    endif()
+endif()
+
 
 # enable crosscompilation
-set(CMAKE_SYSTEM_NAME       Linux)
+set(CMAKE_SYSTEM_NAME       Generic)
 set(CMAKE_SYSTEM_VERSION    1)
 set(CMAKE_CROSSCOMPILING    TRUE)
 
 # specify the cross compiler
 set(CMAKE_SYSTEM_PROCESSOR  ${TARGET_ARCH})
-set(CMAKE_C_COMPILER        ${TARGET_TOOLCHAIN_DIR}/bin/${TARGET_TOOLCHAIN}-gcc)
-set(CMAKE_CXX_COMPILER      ${TARGET_TOOLCHAIN_DIR}/bin/${TARGET_TOOLCHAIN}-g++)
+set(CMAKE_C_COMPILER        ${TARGET_TOOLCHAIN_DIR}/bin/${TARGET_TOOLCHAIN}-gcc${CMAKE_HOST_EXECUTABLE_SUFFIX})
+set(CMAKE_CXX_COMPILER      ${TARGET_TOOLCHAIN_DIR}/bin/${TARGET_TOOLCHAIN}-g++${CMAKE_HOST_EXECUTABLE_SUFFIX})
+
 # skip checking compiler
 set(CMAKE_C_COMPILER_WORKS      1)
 set(CMAKE_CXX_COMPILER_WORKS    1)
+
+# intended for cross-compiling
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 
 # search for programs in the build host directories
